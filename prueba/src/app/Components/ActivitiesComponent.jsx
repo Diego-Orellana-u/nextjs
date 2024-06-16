3f2aeddcceb0b3f0de4cec8b86318f94da623de3
diego orellana <diego.orellana.git@gmail.com>
2024-06-16 15:45:46 -0400

    trying to solve again the new deployment issue

--- prueba/src/app/Components/ActivitiesComponent.jsx
+++ prueba/src/app/Components/ActivitiesComponent.jsx
@@ -8,11 +8,8 @@ import Image from "next/image";
 import { GET } from "../api/activities/route";
 
 export const ActivitiesComponent = async () => {
-  // let req = null;
-  // let activities = null;
-
-  // req = await GET();
-  // activities = await req.json();
+  const req = await GET();
+  const activities = await req.json();
 
   const deleteActivity = async (id) => {
     try {
@@ -27,40 +24,42 @@ export const ActivitiesComponent = async () => {
     } catch (err) {}
   };
 
-  return <div className="flex flex-col border-t-[1px] border-[#555]"></div>;
+  return (
+    <div className="flex flex-col border-t-[1px] border-[#555]">
+      {activities &&
+        activities.map((doc) => (
+          <div
+            key={doc.activityId}
+            className="flex justify-between px-16 border-b-[1px] border-[#555] py-5"
+          >
+            <div>
+              <span>{doc.name}</span>
+            </div>
+            <div className="flex gap-4">
+              {/* todo: create clickable icon component */}
+              <a href={`/${doc.activityId}`}>
+                <Image
+                  src={rightArrow}
+                  alt="right arrow icon"
+                  width={20}
+                  height={20}
+                />
+              </a>
+              <a href={`/edit/${doc.activityId}`}>
+                <Image src={editIcon} alt="edit icon" width={20} height={20} />
+              </a>
+
+              {/* <Image
+          src={deleteIcon}
+          alt="delete icon"
+          className="cursor-pointer"
+          width={17}
+          height={17}
+          onClick={() => deleteActivity(doc.activityId)}
+        /> */}
+            </div>
+          </div>
+        ))}
+    </div>
+  );
 };
-
-// {activities &&
-//   activities.map((doc) => (
-//     <div
-//       key={doc.activityId}
-//       className="flex justify-between px-16 border-b-[1px] border-[#555] py-5"
-//     >
-//       <div>
-//         <span>{doc.name}</span>
-//       </div>
-//       <div className="flex gap-4">
-//         {/* todo: create clickable icon component */}
-//         <a href={`/${doc.activityId}`}>
-//           <Image
-//             src={rightArrow}
-//             alt="right arrow icon"
-//             width={20}
-//             height={20}
-//           />
-//         </a>
-//         <a href={`/edit/${doc.activityId}`}>
-//           <Image src={editIcon} alt="edit icon" width={20} height={20} />
-//         </a>
-
-//         {/* <Image
-//           src={deleteIcon}
-//           alt="delete icon"
-//           className="cursor-pointer"
-//           width={17}
-//           height={17}
-//           onClick={() => deleteActivity(doc.activityId)}
-//         /> */}
-//       </div>
-//     </div>
-//   ))}
