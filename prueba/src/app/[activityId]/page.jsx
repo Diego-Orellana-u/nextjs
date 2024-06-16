a045f514e1241b23a2e0bd2ae3bfb9dcbec88e43
diego orellana <diego.orellana.git@gmail.com>
2024-06-16 03:48:59 -0400

    solved bug in individual activity

--- prueba/src/app/[activityId]/page.jsx
+++ prueba/src/app/[activityId]/page.jsx
@@ -13,14 +13,18 @@ export default async function Activity({ params }) {
 
   return (
     <div className="pt-10 px-10">
-      <a href="/">Back to activities</a>
-      <div className="mt-16 border-[1px] border-red-300 h-[700px] px-10 py-12 text-white">
-        <h2 className="text-2xl mb-4">{activity.name}</h2>
-        <p className="text-base mb-5">{activity.description}</p>
-        <span className="text-sm">
-          Amount of participants: {activity.nParticipants}
-        </span>
-      </div>
+      {activity && (
+        <div>
+          <a href="/">Back to activities</a>
+          <div className="mt-16 border-[1px] border-red-300 h-[700px] px-10 py-12 text-white">
+            <h2 className="text-2xl mb-4">{activity.name}</h2>
+            <p className="text-base mb-5">{activity.description}</p>
+            <span className="text-sm">
+              Amount of participants: {activity.nParticipants}
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
